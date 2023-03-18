83c2bf69ec306f05817ed6f55818091da4c782bb
Carlos Castellanos <castellanosc0520@gmail.com>
2023-03-18 17:42:26 -0400

    Added options

--- ProjectBirthdayList/ProjectBirthdayList/source.c
+++ ProjectBirthdayList/ProjectBirthdayList/source.c
@@ -8,14 +8,23 @@ int main() {
 	printf("Please select an option: \n");
 	printf("List (L), Month (M), Add (A), Remove (R) \n ");
 	scanf_s(" %c", &option, 1);
-	printf("You have selected option: %c", option);
+	printf("You have selected option: %c \n", option);
 
 
 	if (option == 'L') {
 		printf("List");
 	}
 	else if (option == 'M') {
-
+		printf("Month");
+	}
+	else if (option == 'A') {
+		printf("Add");
+	}
+	else if (option == 'R') {
+		printf("Remove");
+	}
+	else {
+		printf("Invalid Option");
 	}
 
 
