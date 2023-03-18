32538485256dc863753be4d79255f84e1dad583a
Carlos Castellanos <89616377+castellanoscc@users.noreply.github.com>
2023-03-18 17:31:40 -0400

    Add files via upload

---
+++ ProjectBirthdayList/ProjectBirthdayList/source.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+	char option;
+
+	printf("Welcome to your Birthday List! \n");
+	printf("Please select an option: \n");
+	printf("List (L), Month (M), Add (A), Remove (R) \n ");
+	scanf_s(" %c", &option, 1);
+	printf("You have selected option: %c", option);
+
+
+	if (option == 'L') {
+		printf("List");
+	}
+	else if (option == 'M') {
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
